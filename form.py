e9eff8900db4be2a33e354429d70270b1b282a73
Richymaster2 <richardkoech69@gmail.com>
2021-01-06 18:03:09 +0300

    Trained model and pickled it

--- form.py
+++ form.py
@@ -4,7 +4,7 @@ from flask_wtf import FlaskForm
 class IrisDatasetForm(FlaskForm):
   SepalLength = StringField('Sepal Length',  [validators.DataRequired()])
   SepalWidth = StringField('Sepal Width', [validators.DataRequired()])
-  PetalWidth = StringField('Petal Width', [validators.DataRequired()])
+  PetalLength = StringField('Petal Length', [validators.DataRequired()])
   PetalWidth = StringField('Petal Width', [validators.DataRequired()])
                  
   submit = SubmitField('Predict')
