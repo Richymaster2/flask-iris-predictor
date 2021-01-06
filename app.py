975f9e280b62c9601be3fd5bf5289f375eed55d6
Richymaster2 <richardkoech69@gmail.com>
2021-01-06 18:17:09 +0300

    added prediction route run into ValueError

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from form import IrisDatasetForm
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
+from decimal import Decimal
+import base64
+import numpy as np
+from io import BytesIO
+
 import pickle
+model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] ="8n48mzuowxelNoezdawnufhui"
@@ -17,11 +26,15 @@ def index():
     form = IrisDatasetForm()
     return render_template('index.html', form=form)
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    
+     int_features = [float(x) for x in request.form.values()]
+     final_features = [np.array(int_features)]
+     prediction = model.predict(final_features)
 
-    return jsonify({"Output": "Predictions from pickled model"})
+     output =prediction[0]
+
+     return render_template('index.html', predicted ='The predicted flower is {}'.format(output))
 
 
 if __name__ == '__main__':
